b931dbfdd625032068eda5f33a8b00c5944cabd1
spectatorMrZ <zhuchunan001@sina.com>
2021-12-03 18:24:26 +0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "CNZone",
+    platforms: [.macOS(.v10_11),
+                .iOS(.v12)
+               ],
+    products: [
+        .library(name: "CNZone",
+                        targets: ["CNZone"])
+    ],
+    dependencies: [
+            .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+            .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0"))
+        ],
+    targets: [
+        .target(name: "CNZone",
+                      dependencies: [
+                        .product(name: "SnapKit", package: "SnapKit"),
+                        .product(name: "CombineMoya", package: "Moya")
+                      ],
+                      path: "Sources"),
+              
+             ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ Sources/CNZone.swift
@@ -0,0 +1,9 @@
+//
+//  CNZone.swift
+//  
+//
+//  Created by 楠 on 2021/12/3.
+//
+
+import Foundation
+
