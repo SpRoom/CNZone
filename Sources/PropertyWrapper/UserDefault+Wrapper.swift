df404d2b75388d5a03b078c577e550e188051f74
spectatorMrZ <zhuchunan001@sina.com>
2022-09-28 12:43:56 +0800

    test:  json encoder crash fix, test 3

--- Sources/PropertyWrapper/UserDefault+Wrapper.swift
+++ Sources/PropertyWrapper/UserDefault+Wrapper.swift
@@ -27,7 +27,7 @@ public struct UserDefault<T: Codable> {
                 let model = try? JSONDecoder().decode(T.self, from: data)
                 return model ?? defaultValue
             } else {
-                if let model = try? JSONSerialization.jsonObject(with: data) as? T {
+                if let model = try? JSONDecoder().decode(T.self, from: data) {
                     return model
                 } else if let dic = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
                     if let model = dic[key] as? T {
@@ -44,8 +44,8 @@ public struct UserDefault<T: Codable> {
                 UserDefaults.standard.set(data, forKey: key)
             } else {
                 //Do the JSONSerialization Thing
-                if JSONSerialization.isValidJSONObject(newValue) {
-                    let data = try? JSONSerialization.data(withJSONObject: newValue, options: [])
+                if let data = try? JSONEncoder().encode(newValue) {
+//                    let data = try? JSONSerialization.data(withJSONObject: newValue, options: [])
                     UserDefaults.standard.set(data, forKey: key)
                 } else {
                     let dic = [key: newValue]
