779974c285b04d24d37b9b256e809b6942062573
spectatorMrZ <zhuchunan001@sina.com>
2023-05-22 00:45:06 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CNZone",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "CNZone",
